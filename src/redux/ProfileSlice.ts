4dde945442b1fb9b8fd5a0409da29f76f5aeb30d
vamshi1810 <vamshi.enaganti1810@gmail.com>
2025-06-04 22:04:28 +0530

    profile changes

--- src/redux/ProfileSlice.ts
+++ src/redux/ProfileSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const API_URL = "https://profile-management-project.onrender.com/profile"
+const API_URL = "http://localhost:5000/profile";
 
 export const fetchProfile = createAsyncThunk(
   "profile/fetchAllProfiles",
